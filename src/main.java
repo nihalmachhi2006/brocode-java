d1c5ab1b8882b6a6301244d7d713500efeaa2ef9
Nihal Machhi <nihalmachhi11@gmail.com>
2026-06-29 19:17:11 +0530

    first commit

---
+++ src/main.java
@@ -0,0 +1,187 @@
+import java.util.Scanner;
+import java.util.Random;
+public class main {
+    public static void main(String[] args){
+//        System.out.print("i like pizza\n");
+//        System.out.print("it's really good");
+
+//        int age = 30;
+//        int year = 2026;
+//        int quantity = 1;
+//        System.out.println("the year is "+ year);
+//        System.out.println(age);
+//        System.out.println(quantity);
+
+//        double price = 10.99;
+//        double gpa = 5.69;
+//        double temperature = -12.95;
+//
+//        System.out.println(price);
+//        System.out.println(gpa);
+//        System.out.println(temperature);
+
+//        char grade = 'A';
+//        char symbol = '!';
+//        char inr = '$';
+//
+//        System.out.println(grade);
+//        System.out.println(symbol);
+//        System.out.println(inr);
+
+//        boolean isStudent = true;
+//        boolean forSale = false;
+//        boolean isOnline = true;
+//
+//        System.out.println(isStudent);
+//
+//        if(isStudent){
+//            System.out.println("you are a student");
+//        }
+//        else{
+//            System.out.println("your are not a student");
+//        }
+
+//        String name = "Nihal";
+//        String food = "pizza";
+//        String email = "notcorrect123@gmail.com";
+//        System.out.println("Hello "+name);
+//        System.out.println("your fav food is "+ food);
+//        System.out.println("your email is "+ email);
+//        Scanner scn = new Scanner(System.in);
+
+//        System.out.print("Enter your name ");
+//        String name = scn.nextLine();
+//
+//
+//        System.out.println("enter your age");
+//        int age = scn.nextInt();
+//
+//        System.out.println("what is your gpd");
+//        double gpa = scn.nextDouble();
+//
+//
+//        System.out.println("hello "+ name);
+//        System.out.println("your age is "+ age);
+//        System.out.println("your gpa is "+ gpa);
+//
+//        double width = 0;
+//        double height = 0;
+//        double area = 0;
+//
+//        System.out.println("Enter your width " );
+//        width = scn.nextDouble();
+//
+//        System.out.println("Enter your height " );
+//        height = scn.nextDouble();
+//
+//        area = width * height;
+//
+//        System.out.println("the are is "+ area);
+
+
+//        String adjective1;
+//        String noun1;
+//        String adjective2;
+//        String verb1;
+//        String adjective3;
+//
+//        System.out.println("enter an adjective (Description)");
+//        adjective1 = scn.nextLine();
+//        System.out.println("enter a noune (animal or  person)");
+//        noun1 = scn.nextLine();
+//        System.out.println("enter an adjective(Description)");
+//        adjective2 = scn.nextLine();
+//        System.out.println("enter a verb end with -ing (action");
+//        verb1 = scn.nextLine();
+//        System.out.println("enter an adjective (Description");
+//        adjective3 = scn.nextLine();
+//
+//        System.out.println("\nToday i went to a" + adjective1 + "zoo");
+//        System.out.println("in an exhibit, i saw a"+ noun1 + ".");
+//        System.out.println("was" + adjective2 + "and"+ verb1 + "!");
+//        System.out.println("i was" + adjective3 + "!");
+
+//        int x = 10;
+//        int y = 2;
+//        int z;
+
+//        z = x + y;
+//        z = x - y;
+//        z = x * y;
+//        z = z / y;
+//        z = z % y;
+//        int x = 1;
+//        int x++; //sem thing as int x = 1;
+//        System.out.println(x);
+
+//        String item;
+//        double price;
+//        int quantity;
+//        char couurency = '$';
+//        double total;
+//
+//        System.out.println("what item would you like to buy: ");
+//        item = scn.nextLine();
+//
+//        System.out.println("what is the price for each?");
+//        price = scn.nextDouble();
+//
+//        System.out.println("how many would you like");
+//        quantity = scn.nextInt();
+//
+//        total = price * quantity;
+//
+//        System.out.println("you have bought"+total);
+
+
+//        int age = 500;
+//
+//        if(age >= 18){
+//            System.out.println("you are an adult");
+//        }
+//        else{
+//            System.out.println("you are a child");
+//        }
+
+//        Random ran = new Random();
+
+//        boolean number;
+//        number = ran.nextBoolean();
+//        System.out.println(number);
+//        System.out.println(Math.PI);
+
+//the print to userinput
+//        int userinput = scn.nextInt();
+
+//        int i = 1;
+//        while(i<userinput){
+//            System.out.println(i+ " ");
+//            i++;
+//        }
+
+//        int i = 1;
+//        int sum = 0;
+//        while(i<=userinput){
+//            sum = (i*i*i);
+//            i++;
+//        }
+//        System.out.println(sum);
+
+
+        // print only even number between 100 to 1000
+
+//        int i = 100;
+//        int count = 0;
+//        while(i<=userinput){
+//            if(i%2==0){
+//                count++;
+//            }
+//            i++;
+//        }
+//        System.out.println(count);
+//
+//
+//        scn.close();
+
+    }
+}
