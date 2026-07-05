6fbb295176763213f1a48a661c73c84f6149abf0
nihalmachhi2006 <nihalmachhi11@gmail.com>
2026-07-05 09:32:30 +0530

    Starting learning basic of java from GFG

---
+++ src/basic.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+public class basic{
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+}
--- src/recursion.java
+++
@@ -1,5 +0,0 @@
-public class recursion {
-    public static void main(String[] args){
-
-    }
-}
