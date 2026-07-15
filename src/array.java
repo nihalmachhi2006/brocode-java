8f12ef6857318d7b305fe7abb0535ee41b36fe49
nihalmachhi2006 <nihalmachhi11@gmail.com>
2026-07-15 11:03:02 +0530

    second largest element

---
+++ src/array.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class array {
+    public static void main(String[] args){
+        int[] arr = new int[]{1,2,5,3,4,7,8,11,25,2,1};
+        int maxsum = -1;
+        int currsum = -1;
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] > maxsum){
+                currsum = maxsum;
+                maxsum = arr[i];
+
+            }
+            else if(arr[i] != maxsum && arr[i] > currsum){
+                    currsum = arr[i];
+                }
+        }
+        System.out.println(currsum);
+    }
+}
