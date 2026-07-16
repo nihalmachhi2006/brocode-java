5d350c98d134976fc07737df76a2428383386881
nihalmachhi2006 <nihalmachhi11@gmail.com>
2026-07-16 13:22:24 +0530

    rev the 2d array

--- src/arrays2d.java
+++ src/arrays2d.java
@@ -19,28 +19,57 @@ public class arrays2d {
 //            }
 //            System.out.println(" ");
 //        }
+//        Scanner scn = new Scanner(System.in);
+//        int n  = scn.nextInt();
+//        int m = scn.nextInt();
+//
+//        int sum = 0;
+//
+//        int[][] ans = new int[n][m];
+//        for(int i = 0; i<ans.length; i++){
+//            for(int j = 0; j<ans.length; j++){
+//                ans[i][j] = scn.nextInt();
+//
+//                sum = sum + ans[i][j];
+//            }
+//        }
+//
+//        for(int i = 0; i<ans.length; i++){
+//            for(int j = 0; j<ans.length; j++){
+//                System.out.print(ans[i][j]+ " ");
+//            }
+//            System.out.println(" ");
+//
+//        }
+//        System.out.println(sum);
         Scanner scn = new Scanner(System.in);
         int n  = scn.nextInt();
         int m = scn.nextInt();
 
-        int sum = 0;
+//        int sum = 0;
 
         int[][] ans = new int[n][m];
         for(int i = 0; i<ans.length; i++){
             for(int j = 0; j<ans.length; j++){
                 ans[i][j] = scn.nextInt();
 
-                sum = sum + ans[i][j];
+//                sum = sum + ans[i][j];
             }
         }
 
         for(int i = 0; i<ans.length; i++){
             for(int j = 0; j<ans.length; j++){
-                System.out.print(ans[i][j]+ " ");
+//                System.out.print(ans[i][j]+ " ");
+                if(i%2 == 0){
+                    System.out.print(ans[i][j]+ " ");
+                }
+                else{
+                    System.out.print(ans[i][ans[i].length - 1 - j] + " ");
+                }
             }
             System.out.println(" ");
 
         }
-        System.out.println(sum);
+//        System.out.println(sum);
     }
 }
