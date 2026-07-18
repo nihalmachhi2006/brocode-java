2c0ef95ca6fc92e4fa04e8e355150db19cbddb1e
nihalmachhi2006 <nihalmachhi11@gmail.com>
2026-07-18 13:17:21 +0530

    learn overloading in java

--- OOPS/src/Main.java
+++ OOPS/src/Main.java
@@ -1,7 +1,20 @@
 import java.util.*;
 
+//class sum{
+//    int add(int a, int b){
+//        return a+b;
+//    }
+//    int add(int a, int b, int c){
+//        return a+b+c;
+//    }
+//}
+
+
 class main{
     public static void main(String[] args){
-        System.out.println("Starting OOPS ");
+//        System.out.println("Starting OOPS ");
+//         sum sm = new sum();
+//        System.out.println(sm.add(10,20));
+//        System.out.println(sm.add(10,20,30));
     }
 }
