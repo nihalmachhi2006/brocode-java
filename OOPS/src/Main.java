98a0e1bd1245d30a704af71a8a4779f88e611c74
nihalmachhi2006 <nihalmachhi11@gmail.com>
2026-07-19 08:46:41 +0530

    Starting learning java from start : Basic

--- OOPS/src/Main.java
+++ OOPS/src/Main.java
@@ -1,28 +1,7 @@
 import java.util.*;
 
-class animal{
-    void sound(){
-        System.out.println("someting make sound");
-    }
-}
-class dog extends animal{
-    void sound(){
-        System.out.println("dog is dark");
-    }
-}
-class cat extends animal{
-    void sound(){
-        System.out.println("cat is dark");
-    }
-}
-
 class main{
     public static void main(String[] args){
-        animal a;
-
-        a = new dog();
-        a.sound();
-        a = new cat();
-        a.sound();
+//        System.out.println("Nihal Machhi");
     }
 }
