8f333edfbd76fd21b43f08c30134593b9df48e03
nihalmachhi2006 <nihalmachhi11@gmail.com>
2026-08-06 14:40:39 +0530

    start basic agian

--- src/main.java
+++ src/main.java
@@ -2,17 +2,26 @@ import java.util.*;
 
 class Main {
     public static void selectionSort(int n, int[] arr){
-        for(int i = 0; i < n-1; i++){
-            int min = i;
-            for(int j = i+1; j < n; j++){
-                if(arr[min] > arr[j]){
-                    min = j;
-                }
+//        for(int i = 0; i < n-1; i++){
+//            int min = i;
+//            for(int j = i+1; j < n; j++){
+//                if(arr[min] > arr[j]){
+//                    min = j;
+//                }
+//            }
+//
+//            int temp = arr[i];
+//            arr[i] = arr[min];
+//            arr[min] = temp;
+//        }
+        for(int i = 1; i < n; i++){
+            int key = arr[i];
+            int j = i - 1;
+            while(j  >= 0 && arr[j] > key){
+                arr[j+1] = arr[j];
+                j--;
             }
-
-            int temp = arr[i];
-            arr[i] = arr[min];
-            arr[min] = temp; 
+            arr[j+1] = key;
         }
     }
 
