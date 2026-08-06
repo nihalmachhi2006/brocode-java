7fad90df6bd7f399783db6339153fe98e95d5dc2
nihalmachhi2006 <nihalmachhi11@gmail.com>
2026-08-06 15:45:02 +0530

    TODO for next 3 days

--- src/main.java
+++ src/main.java
@@ -1,36 +1,9 @@
-import java.util.*;
+import java.util.Scanner;
 
-class Main {
-    public static void selectionSort(int n, int[] arr){
-//        for(int i = 0; i < n-1; i++){
-//            int min = i;
-//            for(int j = i+1; j < n; j++){
-//                if(arr[min] > arr[j]){
-//                    min = j;
-//                }
-//            }
-//
-//            int temp = arr[i];
-//            arr[i] = arr[min];
-//            arr[min] = temp;
-//        }
-        for(int i = 1; i < n; i++){
-            int key = arr[i];
-            int j = i - 1;
-            while(j  >= 0 && arr[j] > key){
-                arr[j+1] = arr[j];
-                j--;
-            }
-            arr[j+1] = key;
-        }
-    }
+class main{
+    public static void main(String[] args){
+        //TODO: learn each and every DS and its CURD operations
 
-    public static void main(String[] args) {
-        int n = 5;
-        int[] arr = {64,22, 34, 11, 12};
-        selectionSort(n, arr);
-        for(int elem : arr){
-            System.out.print(elem + " ");
-        }
+
     }
 }
